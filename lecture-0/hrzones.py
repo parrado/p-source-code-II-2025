b7c234d8a18be6f05531127fd05e5f9ec5f5d51c
Alexander López-Parrado <parrado@uniquindio.edu.co>
2025-08-12 16:15:39 -0500

    Add files via upload

---
+++ lecture-0/hrzones.py
@@ -0,0 +1,44 @@
+# Ask user for age
+age=int(input('Enter your age: '))
+
+# Estimate maximum heart rate based on age
+m=208-0.7*age
+
+# Print estimation of maximum heart rate
+print(f'Your max heart rate is {m} bpm')
+
+# Ask for number of workouts
+n=int(input('Enter number of workouts: '))
+
+# Make room for heart rate values of each workout
+hrs=[0.0]*n
+
+# For loop
+for i in range(n):
+    hr=int(input(f'Enter HR for workout {i+1} in bpm: '))        
+    hrs[i]=hr
+
+zones=[]
+i=0
+for hr in hrs:
+    if (0.5*m)<=hr<(0.6*m):
+        zones.append('z1')
+   
+    if (0.6*m)<=hr<(0.7*m):
+        zones.append('z2')
+      
+    if (0.7*m)<=hr<(0.8*m):
+        zones.append('z3')
+
+    if (0.8*m)<=hr<(0.9*m):        
+        zones.append('z4')
+
+    if (0.9*m)<=hr<=m:
+        zones.append('z5')
+    
+    print(f'Workout {i+1} was in {zones[i]}')
+    i+=1
+
+
+
+
